d0087a17960e99ddf87dc6dde57a441cf5279682
Yannik Zimmermann <yannik.z@me.com>
2020-10-19 16:35:14 +0200

    Add basic mapview example from https://www.hackingwithswift.com/books/ios-swiftui/advanced-mkmapview-with-swiftui

---
+++ Pictation/MapView.swift
@@ -0,0 +1,52 @@
+//
+//  MapView.swift
+//  Pictation
+//
+//  Created by Yannik Zimmermann on 19.10.20.
+//
+// Mapview example provided by https://www.hackingwithswift.com/books/ios-swiftui/advanced-mkmapview-with-swiftui
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    @Binding var centerCoordinate: CLLocationCoordinate2D
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+    }
+}
+
+extension MKPointAnnotation {
+    static var example: MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Home to the 2012 Summer Olympics."
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: -0.13)
+        return annotation
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(centerCoordinate: .constant(MKPointAnnotation.example.coordinate))
+    }
+}
